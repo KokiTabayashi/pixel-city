0113d359cb8dcd409829936319513e8fbd6dd387
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-04-29 22:20:17 -0700

    lec 108

---
+++ pixel-city/Controller/PopVC.swift
@@ -0,0 +1,38 @@
+//
+//  PopVC.swift
+//  pixel-city
+//
+//  Created by Koki Tabayashi on 2018/04/29.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController, UIGestureRecognizerDelegate {
+
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    var passedImage: UIImage!
+    
+    func initData(forImage image: UIImage) {
+        self.passedImage = image
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        popImageView.image = passedImage
+        addDoubleTap()
+    }
+    
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func screenWasDoubleTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
